2cc39787b3c83957f7b8fe8a6c52d11ae5815d47
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-01-22 21:25:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+list_spkrs = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+return "Hello, my name is #{name}"
+end
+
+def batch_badge_creator(list_spkrs)
+
+  badge_msg = []
+list_spkrs.each do |spkrs|
+  badge_msg << badge_maker(spkrs)
+end
+badge_msg
+end
+
+def assign_rooms(list_spkrs)
+  rooms = []
+  array.each _with_index do |name, counter|
+    
+rooms << "Hello, #{name}! You have been assigned to room #{counter+ 1}!"
+end
+rooms
+end
